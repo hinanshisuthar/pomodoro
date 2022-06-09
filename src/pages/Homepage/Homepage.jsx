8a1adb05fb3d119b360023a1ade38fe94b6e7043
Hinanshi Suthar <hinanshis4@gmail.com>
2022-06-09 17:09:34 +0530

    [Feat] Homepage

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
-import {
-  AiFillPlusCircle,
-  BsFillTrashFill,
-  BsPencilSquare,
-} from "../../components/icons";
+import { NewTask } from "../../components/NewTask";
+import { TaskList } from "../../components/TaskList";
+import { useData } from "../../context/data-context";
 import "./homepage.css";
 
 const Homepage = () => {
+  const {
+    tasklist,
+  } = useData();
+
   return (
     <div className="wrapper-center grid vrt-30-70 p-2">
       <div className="top-box p-sm">
@@ -14,40 +16,16 @@ const Homepage = () => {
           <h1 className="text-lgt p-sm">
             Welcome back, <span className="highlight">User</span> !
           </h1>
-          <Link to="/login" >
+          <Link to="/login">
             <button className="btn btn-primary br-8">My Account</button>
           </Link>
         </div>
         <h4 className="text-lgt p-sm">Login to see, your tasks !</h4>
       </div>
       <div className="btm-box br-10">
-        <div className="task-div flex-row-sb">
-          <h3 className="text-bold">To - Do List</h3>
-          <AiFillPlusCircle size={30} className="icon" />
-        </div>
+        <NewTask />
         <div className="task-container p-md">
-          <div>
-            <div className="flex-row-sb p-sm">
-              <input type="checkbox" name="task" />
-              <label>
-                <h4 className="task-name p-sm">Task 1</h4>
-              </label>
-              <div className="ml-auto">
-                <BsPencilSquare size={22} className="icon mx-sm" />
-                <BsFillTrashFill size={22} className="icon mx-sm" />
-              </div>
-            </div>
-            <div className="flex-row-sb p-sm">
-              <input type="checkbox" name="task" />
-              <label>
-                <h4 className="task-name p-sm">Task 2</h4>
-              </label>
-              <div className="ml-auto">
-                <BsPencilSquare size={22} className="icon mx-sm" />
-                <BsFillTrashFill size={22} className="icon mx-sm" />
-              </div>
-            </div>
-          </div>
+          <TaskList tasklist={tasklist} />
         </div>
       </div>
     </div>
