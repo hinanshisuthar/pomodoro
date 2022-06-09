753423bc3e17bd43d8133ea72138e75a8f278b35
Hinanshi Suthar <hinanshis4@gmail.com>
2022-06-09 17:09:23 +0530

    [Feat] Context

---
+++ src/context/data-context.js
@@ -0,0 +1,92 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import { db } from "../firebase-config";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore";
+import {useNavigate} from "react-router-dom";
+
+const DataContext = createContext();
+
+const DataProvider = ({ children }) => {
+  const [tasklist, setTasklist] = useState([]);
+  const [taskInputs, setTaskInputs] = useState({
+    title: "",
+  });
+  const taskCollection = collection(db, "tasks");
+  const [isEditing, setIsEditing] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const data = await getDocs(taskCollection);
+      setTasklist(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getTasks();
+    // eslint-disable-next-line
+  }, []);
+
+  const createTask = async () => {
+    const docRef = await addDoc(taskCollection, {
+      title: taskInputs.title,
+    });
+    setTasklist((val) => [
+      ...val,
+      {
+        id: docRef.id,
+        title: taskInputs.title,
+      },
+    ]);
+    setTaskInputs({
+      title: "",
+    });
+  };
+
+  const deleteTask = async (id) => {
+    const selectedDoc = doc(db, "tasks", id);
+    await deleteDoc(selectedDoc);
+    const listAfterDelete = tasklist.filter((task) => task.id !== id);
+    setTasklist(listAfterDelete);
+  };
+
+  const editTask = async (id) => {
+    const selectedDoc = doc(db, "tasks", id);
+    const dataToUpdate = {
+      title: taskInputs.title,
+    };
+    await updateDoc(selectedDoc, dataToUpdate);
+    const listAfterUpdate = tasklist.map((ele) => ele.id === id ? {...taskInputs, id: id, updatedOn: new Date()} : ele);
+    setTasklist(listAfterUpdate);
+    setIsEditing(false);
+    setTaskInputs({
+      title: "",
+    });
+    navigate("/")
+  };
+
+  return (
+    <DataContext.Provider
+      value={{
+        tasklist,
+        setTasklist,
+        taskInputs,
+        setTaskInputs,
+        createTask,
+        deleteTask,
+        editTask,
+        isEditing,
+        setIsEditing,
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
+};
+
+const useData = () => useContext(DataContext);
+
+export { DataProvider, useData };
